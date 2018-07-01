67ec0ce38e6422e3e91978faaa522ceb69f9883c
gui camargo <gui551@hotmail.com>
2018-07-01 17:50:09 -0300

    Teste derivada parcial

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,9 +2,11 @@ package main;
 import org.mariuszgromada.math.mxparser.Argument;
 import org.mariuszgromada.math.mxparser.Expression;
 import org.mariuszgromada.math.mxparser.Function;
+import org.mariuszgromada.math.mxparser.mXparser;
 
 import Mono.Metodos;
 import MultiVariable.SolveMinimum;
+import Tool.ExpressionMath;
 import Tool.FunctionMath;
 
 public class Main {
@@ -14,11 +16,14 @@ public class Main {
 		Metodos.BuscaDicotomica("x^2 + 2", -3, 6, 0.01, 0.1);
 		Metodos.BuscaFibonacci("x^2 - 3*x + 2", -1, 3, 0.1);
 		Metodos.BuscaFibonacci("x*sin(4*x)", 0, 3, 0.1);
-		System.out.println( Metodos.Bisseccao("((1.6088 + x*1.0) - 2)^4 + ((1.6088 + x*1.0) - 2*(0.8479 + x*0.0))^2 + 0.1*((1.6088 + x*1.0)^2 - (0.8479 + x*0.0))^2", -5, 0, 0.0001) );
-		//System.out.println( Metodos.Newton("x^2 - 3*x +2 ", -1, 3, 0.01) );
+	
 		
-		double[] a= {0.9904 , 0.8421};
-		SolveMinimum.CoordenadasCiclicas("f(x1,x2) = (x1 - 2)^4 + (x1 - 2*x2)^2 + 100*(x1^2 - x2)^2", a, 0.0001);
+		/* Function */
+		Function f = new Function("f(x1,x2) = der(x1^2 + x2, x2)");	         
+		mXparser.consolePrintln("Res : " + f.getFunctionExpressionString() + " = " + f.calculate(5,96));
+
+		/*double[] a= {1 , 0};
+		SolveMinimum.CoordenadasCiclicas("f(x1,x2) = x1^2 + 2*x2 + 0.1*(x1^2 + x2^2 - 1)^2", a, 0.0001);*/
 		
 	}
 
